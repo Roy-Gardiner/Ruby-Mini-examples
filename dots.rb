2ddb6f3e53c51c3e1ac53ae5009a609857fb44e7
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-01-13 15:25:38 +0000

    priority of methods simplified

--- dots.rb
+++ dots.rb
@@ -1,9 +1,20 @@
 #!/Users/roygardiner/.rvm/rubies/ruby-2.0.0-p353/bin/ruby
 
+# it looks like the . 'operator' or whatever it is, isn't pure left-to-right.  How does it work?  
+# Why wouldn't it be simple l-t-r to minimise use of brackets?
 
 i = 10
-puts ">#{((i+1).to_s + ":").ljust(5)}<" 
-puts ">#{(i+1).to_s + ":".ljust(5)}<" 
-puts ">#{i.to_s + ":".ljust(5)}<" 
 
-puts ">#{i+1.to_s + ":".ljust(5)}<" 
+
+puts ">#{('a' + ":").ljust(5)}<" 
+puts ">#{'a' + ":".ljust(5)}<" 
+
+#puts ">#{('a' + ":")}<" 
+#puts ">#{'a' + ":"}<" 
+#
+#colon = ":".ljust(5)
+#puts ">#{'a'.+(colon) }<" 
+
+#puts ">#{i.to_s + ":".ljust(5)}<" 
+#
+#puts ">#{i+1.to_s + ":".ljust(5)}<" 
